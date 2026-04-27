3f042831b70d478d261a03dd385196f4ebea3ae6
cellolle <marcellopandiangan@gmail.com>
2026-04-27 09:06:03 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-    return 0;
+typedef struct perhitungan {
+  int size;
+  int arr[];
+}
+contoh;
+
+int main() {
+  int n;
+  int sum;
+  int count;
+  int sorted;
+  int median;
+  int n1;
+  int n2;
+  int min;
+  
+  contoh *data = (contoh * ) malloc(sizeof(contoh * ) + n * sizeof(int));
+  n = 0;
+  while (data -> arr[n-1] != -1)
+  {
+    scanf("%d", data -> arr + n);
+    n = n + 1;
+  }
+
+  
+  if ((n-1) % 2 == 0)
+  {
+    n1 = (n-1) / 2;
+    n2 = ((n-1) /2) + 1;
+    median = (data -> arr[n1] + data -> arr[n2]) / 2;
+    
+  }
+  else {
+    n1 = (n-1) / 2;
+    median = data -> arr[n1];
+  }
+
+  for (int i = 0; i < (n-1) - 1; i++) {
+        int min = i;
+        for (int j = i + 1; j < n-1; j++) {
+            if (data -> arr[j] < data-> arr[min])
+                min = j;
+        }
+       if (min != i) {
+            int temp =  data -> arr[min];
+            data -> arr[min] = data -> arr[i];
+            data -> arr[i] = temp;
+        }
+    }
+
+count = n-1;
+printf("COUNT %d", count);
+printf(" SORTED ");
+  for (int i = 0; i < n-1; i++)
+    printf("%d ", data -> arr[i]);
+printf(" MEDIAN %d ", median);
+  free(data);
+  return 0;
 }
